bfca2e0a04ce191c3d816261bcb68320c76e95bc
Patrick Lang <patrick@Patricks-Laptop.local>
2024-08-15 18:33:27 -0400

    move resume button

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -20,9 +20,10 @@ export default function About() {
             <p>
             At MassMutual I worked on a fast-paced agile team. I helped develop, deploy and improve ServiceNet 2.0, a web application used by financial professionals to service their clients' policies and contracts. I honed my React skills to build out application features, integrated numerous API calls to enable new transactions within our application, and helped coordinate a variety of technology integrations and migrations, such as config changes for new landing zones, self-hosted Kafka to Kafka Confluent Cloud migration, and Legacy Vault to Vault SaaS migration.
           </p>
+          <a href={resume} download><button>Check out my Resume!</button></a>
           <h2>This portfolio</h2>
           <p>I'm a self-taught developer. This portfolio was originally created in 2021 and 2022 while I was landing my first coding job. Though it has lain dormant for a while, I am resurrecting it to serve as a visual focus for my job-search efforts. I won't be putting all projects back online, as they need to be transferred from create-react-app to vite, moved from Heroku to Render.io (RIP Heroku free tier) and have their outdated dependecies update. Needless to say, after several years working professionally, I would approach some of these projects differently that I did before, but I'm still proud of my work back then!</p>
-                    <a href={resume} download><button>Check out my Resume!</button></a>
+                    
                 </div>
                   
                  
