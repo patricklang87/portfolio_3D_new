0322ac44ec4bc6c1714de1789e9084ab7595bb8c
Patrick Lang <patrick@Patricks-Laptop.local>
2024-08-16 09:20:38 -0400

    update nodemailer auth

--- routes/api/mail.js
+++ routes/api/mail.js
@@ -1,14 +1,17 @@
+// enabling Nodemailer using Google Gmail API: https://shazaali.substack.com/p/using-oauth2-with-nodemailer-for
+
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
+        type: 'OAuth2',
         user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
     }
 });
 
